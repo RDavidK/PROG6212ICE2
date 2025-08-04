e4a0cff60bc756745df1a28ddc2d26d003aa2146
ZandreKoekemoer <ST10445246@vcconnect.edu.za>
2025-08-04 13:32:51 +0200

    Update Ice Part 2

--- ICE2Indexers/Program.cs
+++ ICE2Indexers/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Pokemon Delta = new Pokemon("Metapod", "Bug", "Shed Skin", "Rock, Fire, Flying", 21.8, 2.04);
+
+            Console.WriteLine("Pokemon name: " + Delta[0]);
+            Console.WriteLine("Pokemon type: " + Delta[1]);
+            Console.WriteLine("Pokemon ability: " + Delta[2]);
+            Console.WriteLine("Pokemon weaknesses: " + Delta[3]);
+            Console.WriteLine("Pokemon weight: " + Delta[4] + " lbs");
+            Console.WriteLine("Pokemon height: " + Delta[5] + " ft");
+            
+
         }
     }
 }
--- Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3608bfa93c6025f0a558467dce20d24f45dee132")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
